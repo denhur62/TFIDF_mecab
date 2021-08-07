9de8eff980ae43abc2f892ed34d814c4e8ff4378
denhur62 <denhur62@naver.com>
2021-08-08 00:49:41 +0900

    add tf.pickle and restore

--- TFIDF.py
+++ TFIDF.py
@@ -4,13 +4,16 @@ from sklearn.metrics.pairwise import linear_kernel
 from PartitionFit import partial_fit
 from Tokenizer import tokenizer,stopwords
 from DataLoader import data_loader
-
+import _pickle as pickle
+import scipy.sparse as sp
+TfidfVectorizer.partial_fit = partial_fit
 tf = TfidfVectorizer(
     stop_words=stopwords, tokenizer=tokenizer, ngram_range=(1, 2), max_features = 200000, sublinear_tf=True)
-TfidfVectorizer.partial_fit = partial_fit
+
 
 catergory=['book','concern','daliy','excercise','hobby','love','movie','pet','restaurant','study','travel']
 mtx_path='model/model.mtx'
+tf_path='model/tf.pickle'
 
 def first_learning(tf,mtx_path):
     article=[]
@@ -19,21 +22,32 @@ def first_learning(tf,mtx_path):
         article+=data
     X = tf.fit_transform(article)
     tf.n_docs = len(article)
-    mmwrite(mtx_path,X)
+    with open(mtx_path,"wb") as fw:
+        pickle.dump(X,fw)
+    with open(tf_path,"wb") as fw:
+        pickle.dump(tf,fw)
 
 def after_learning(tf,more):
     tf.partial_fit([more])
 
-def recommand(tf,more):
-    after_learning(tf,more)
-    X = mmread(mtx_path).tocsr()
-    example_vector = tf.fit_transform([more])
+def recommand(more):
+    with open(mtx_path, "rb") as fr:
+        X=pickle.load(fr)
+    with open(tf_path,"rb") as fr:
+        tf=pickle.load(fr)
+    # X = mmread(mtx_path).tocsr()
+    example_vector = tf.transform([more])
+    tf.partial_fit([more])
+    with open(tf_path,"wb") as fw:
+        pickle.dump(tf,fw)
     print(example_vector.shape)
     print(X.shape)
     cos_similar= linear_kernel(example_vector,X).flatten()
+    
     sim_rank_idx=cos_similar.argsort()[::-1]
     sim_rank_idx=sim_rank_idx[:5]
     for i in sim_rank_idx:
+        print()
         print(cos_similar[i])
     
     
@@ -41,7 +55,5 @@ def recommand(tf,more):
 
 # X = mmread(mtx_path).tocsr() (22515,441976)
 
-recommand(tf,'오늘은 책을 읽고 싶은 밤이다. 일기를 쓰려고 누웠는데 책이 눈에 들어왔다. 내돈내산하고 작성하는 책 리뷰 여행을 가고자 하였다.')
-
-
+recommand('오늘은 책을 읽고 싶은 밤이다. 일기를 쓰려고 누웠는데 책이 눈에 들어왔다. 내돈내산하고 작성하는 책 리뷰 여행을 가고자 하였다.')
 
